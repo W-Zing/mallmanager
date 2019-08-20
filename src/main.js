c33e773ac6991678abc1c25f26f77ff7e41a06e1
WenWang <15898400711@163.com>
2019-08-20 19:40:15 +0800

    引入提示框

--- src/main.js
+++ src/main.js
@@ -5,15 +5,18 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import '@/assets/css/reset.css'
+import MyServerHttp from '@/plugins/http.js'
 
-Vue.use(ElementUI);
+Vue.use(ElementUI)
+Vue.use(MyServerHttp)
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+    el: '#app',
+    router,
+    components: { App },
+    template: '<App/>'
 })
